15a245926356176db1044374a39cee1b5ed1cecb
ALI <aliabdullaev2021@gmail.com>
2025-12-29 08:13:05 +0000

    Codeany RFO2023 Grade9 first guestion

--- codeany/RFO2023/Round1/Grade9/first.cpp
+++ codeany/RFO2023/Round1/Grade9/first.cpp
@@ -1,7 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string s;
+string vowels{"AEIOUY"};
+int last_position = 0;
+int current_position = 0;
+int jump_len = 0;
+
 int main()
 {
-    
+    cin >> s;
+    s.insert(0,1,'A');
+    s.push_back('A');
+
+    for(int i = 1; i<=s.size(); i++)
+    {
+
+        if(vowels.find(s[i]) != string::npos)
+        {
+            last_position = current_position;
+            current_position = i;
+            if((current_position - last_position) > jump_len) jump_len = current_position - last_position;
+        }
+   }
+
+    printf("%d\n",jump_len);
 }
