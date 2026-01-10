fc9424f1d6c7c1052a45c70567209820bdb075f5
ALI <aliabdullaev2021@gmail.com>
2026-01-10 10:23:24 +0000

    fgyui

---
+++ real_competititons/RFO2026-round2/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+int main()
+{
+    scanf("%d",&t);
+    for(int i = 0;i<t;i++)
+    {
+        int n;
+        scanf("%d",&n);
+        string s;
+        cin >> s;
+        size_t pos = s.find("4");
+        if(pos !=string::npos && n-pos >=9)
+        {
+            printf("YES");
+        }
+        else
+        {
+            printf("NO");
+        }
+    }
+    
+}
