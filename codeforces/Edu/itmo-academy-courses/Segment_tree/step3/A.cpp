0bc10db35d0ae434ce7d511e87b4320ad71e1424
adbullaev <aliabdullaev307@gmail.com>
2026-03-19 00:54:17 +0400

    some minor file structuring was done

---
+++ codeforces/Edu/itmo-academy-courses/Segment_tree/step3/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long n;
+
+void build(long long v,long long l,long long r);
+int main()
+{
+    scanf("%lld",&n);
+    vector<long long> vec(n);
+    vector<long long> T(4*n);
+    for(long long i=0;i<n;i++)
+    {
+        scanf("%lld",&vec[i]);
+    }
+
+
+
+}
